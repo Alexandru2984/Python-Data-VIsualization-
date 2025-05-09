81e18c0cdafafc4739f1728e55933a069848c6c6
Dragne Alexandru Mihai <alex_mihai984@yahoo.com>
2025-05-09 20:00:19 +0300

    asf

--- CURS_1_09.05.2025/5.exercitiu_pie_chart.py
+++ CURS_1_09.05.2025/5.exercitiu_pie_chart.py
@@ -1,6 +1,20 @@
 import matplotlib.pyplot as plt
 import csv
 
-with open("tari.csv")
+countries = []
+population = []
+with open("tari.csv") as freader:
+    csv_reader = csv.DictReader(freader)
+    print(csv_reader)
+
+    for row in csv_reader:
+        countries.append(row["country"])
+        population.append(int(row["population"]))
+
+print("Countries", countries)
+print("Population", population)
+plt.title("Populatia celor mai mari 10 tari")
+plt.pie(population, labels=countries);
+plt.savefig("10_tari.png")
 
 
